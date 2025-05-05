702029421379eaa840338daa6cabf119e46998f0
chardi90 <chardi90@hotmail.com>
2025-05-05 19:56:20 +0100

    Rearranged JavaScript to keep all functions separate at the bottom

--- src/index.js
+++ src/index.js
@@ -1,53 +1,7 @@
 function refreshWeather(response) {
-  function formatDate(date) {
-    let minutes = date.getMinutes();
-    let hours = date.getHours();
-    let day = date.getDay();
-    let number = date.getDate();
-    let month = date.getMonth();
-
-    if (minutes < 10) {
-      minutes = `0${minutes}`;
-    }
-    if (hours < 10) {
-      hours = `0${hours}`;
-    }
-
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-
-    let months = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ];
-
-    let formattedDay = days[day];
-    let formattedMonth = months[month];
-    return `${formattedDay}, ${number} ${formattedMonth} ${hours}:${minutes}`;
-  }
-
-  let currentDateElement = document.querySelector("#current-date-time");
-  let date = new Date();
-  currentDateElement.innerHTML = formatDate(date);
-
   console.log(response.data);
+  let currentDateElement = document.querySelector("#current-date-time");
+  let date = new Date(response.data.time * 1000);
   let cityElement = document.querySelector("#city-name");
   let currentCity = response.data.city;
   let currentCountry = response.data.country;
@@ -59,42 +13,16 @@ function refreshWeather(response) {
   let currentDescription = response.data.condition.description;
   let currentWindSpeedElement = document.querySelector("#wind-speed-today");
   let currentWindSpeed = response.data.wind.speed;
-
-  function formatWindDirection(degree) {
-    if (degree >= 348.75 || degree < 11.25) return "N";
-    if (degree >= 11.25 && degree < 33.75) return "NNE";
-    if (degree >= 33.75 && degree < 56.25) return "NE";
-    if (degree >= 56.25 && degree < 78.75) return "ENE";
-    if (degree >= 78.75 && degree < 101.25) return "E";
-    if (degree >= 101.25 && degree < 123.75) return "ESE";
-    if (degree >= 123.75 && degree < 146.25) return "SE";
-    if (degree >= 146.25 && degree < 168.75) return "SSE";
-    if (degree >= 168.75 && degree < 191.25) return "S";
-    if (degree >= 191.25 && degree < 213.75) return "SSW";
-    if (degree >= 213.75 && degree < 236.25) return "SW";
-    if (degree >= 236.25 && degree < 258.75) return "WSW";
-    if (degree >= 258.75 && degree < 281.25) return "W";
-    if (degree >= 281.25 && degree < 303.75) return "WNW";
-    if (degree >= 303.75 && degree < 326.25) return "NW";
-    if (degree >= 326.25 && degree < 348.75) return "NNW";
-  }
   let currentWindDirectionElement =
     document.querySelector("#wind-degree-today");
   let degree = response.data.wind.degree;
   let currentWindDirection = formatWindDirection(degree);
   let currentHumidityElement = document.querySelector("#humidity-today");
   let currentHumidity = response.data.temperature.humidity;
-
-  function formatPressure(pressure) {
-    if (pressure >= 1020) return "High";
-    if (pressure >= 1000 && pressure < 1020) return "Normal";
-    if (pressure >= 980 && pressure < 1000) return "Low";
-    if (pressure >= 0 && pressure < 980) return "Very low";
-  }
   let currentPressureElement = document.querySelector("#pressure-today");
   let pressure = Number(response.data.temperature.pressure);
   let currentPressure = formatPressure(pressure);
-
+  currentDateElement.innerHTML = formatDate(date);
   cityElement.innerHTML = `${currentCity}, ${currentCountry}`;
   currentTemperatureElement.innerHTML = Math.round(currentTemperature);
   currentFeelsElement.innerHTML = Math.round(currentFeelsLike);
@@ -103,6 +31,76 @@ function refreshWeather(response) {
   currentWindDirectionElement.innerHTML = currentWindDirection;
   currentHumidityElement.innerHTML = `${currentHumidity}%`;
   currentPressureElement.innerHTML = `${currentPressure} (${pressure})`;
+}
+
+function formatPressure(pressure) {
+  if (pressure >= 1020) return "High";
+  if (pressure >= 1000 && pressure < 1020) return "Normal";
+  if (pressure >= 980 && pressure < 1000) return "Low";
+  if (pressure >= 0 && pressure < 980) return "Very low";
+}
+
+function formatWindDirection(degree) {
+  if (degree >= 348.75 || degree < 11.25) return `N (${degree})`;
+  if (degree >= 11.25 && degree < 33.75) return `NNE (${degree})`;
+  if (degree >= 33.75 && degree < 56.25) return `NE (${degree})`;
+  if (degree >= 56.25 && degree < 78.75) return `ENE (${degree})`;
+  if (degree >= 78.75 && degree < 101.25) return `E (${degree})`;
+  if (degree >= 101.25 && degree < 123.75) return `ESE (${degree})`;
+  if (degree >= 123.75 && degree < 146.25) return `SE (${degree})`;
+  if (degree >= 146.25 && degree < 168.75) return `SSE (${degree})`;
+  if (degree >= 168.75 && degree < 191.25) return `S (${degree})`;
+  if (degree >= 191.25 && degree < 213.75) return `SSW (${degree})`;
+  if (degree >= 213.75 && degree < 236.25) return `SW (${degree})`;
+  if (degree >= 236.25 && degree < 258.75) return `WSW (${degree})`;
+  if (degree >= 258.75 && degree < 281.25) return `W (${degree})`;
+  if (degree >= 281.25 && degree < 303.75) return `WNW (${degree})`;
+  if (degree >= 303.75 && degree < 326.25) return `NW (${degree})`;
+  if (degree >= 326.25 && degree < 348.75) return `NNW (${degree})`;
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+  let number = date.getDate();
+  let month = date.getMonth();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  let formattedDay = days[day];
+  let formattedMonth = months[month];
+  return `${formattedDay}, ${number} ${formattedMonth} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {

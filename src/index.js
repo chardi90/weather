6de40c0cdf6400d0f7161209e441e12e6085b7d9
chardi90 <chardi90@hotmail.com>
2025-05-05 19:38:00 +0100

    Removed hard HTML for some elements & added formattime JS
    Time and date will change to current time and date

--- src/index.js
+++ src/index.js
@@ -1,4 +1,52 @@
 function refreshWeather(response) {
+  function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let day = date.getDay();
+    let number = date.getDate();
+    let month = date.getMonth();
+
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+
+    let formattedDay = days[day];
+    let formattedMonth = months[month];
+    return `${formattedDay}, ${number} ${formattedMonth} ${hours}:${minutes}`;
+  }
+
+  let currentDateElement = document.querySelector("#current-date-time");
+  let date = new Date();
+  currentDateElement.innerHTML = formatDate(date);
+
   console.log(response.data);
   let cityElement = document.querySelector("#city-name");
   let currentCity = response.data.city;

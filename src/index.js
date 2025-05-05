9f574065079d9a5018f067fbc9a3b73bf0122a58
chardi90 <chardi90@hotmail.com>
2025-05-05 17:47:54 +0100

    Improved JS functionality for city search results
    Made city, country and temp change to search result
    Remove hard HTML for city and temp
    Added Manchester as inititial search result

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
+function refreshWeather(response) {
+  let cityElement = document.querySelector("#city-name");
+  let currentCity = response.data.city;
+  let currentCountry = response.data.country;
+  let currentTemperatureElement = document.querySelector("#todays-temp");
+  let currentTemperature = response.data.temperature.current;
+
+  cityElement.innerHTML = `${currentCity}, ${currentCountry}`;
+  currentTemperatureElement.innerHTML = Math.round(currentTemperature);
+}
+
+function searchCity(city) {
+  let apiKey = "a2t477eebb3f98daaa0d6cf85ob51907";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#city-name");
-  let countryElement = document.querySelector("#country-name");
-  let currentTemperatureElement = document.querySelector("#todays-temp");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#city-search");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Manchester");

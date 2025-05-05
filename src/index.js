e940a7e3f9d436189470d31c16b92c055bba9d89
chardi90 <chardi90@hotmail.com>
2025-05-05 18:20:25 +0100

    Improved weather search function
    Added JS for current weather incl. wind, conditions, pressure, humidity etc.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 function refreshWeather(response) {
+  console.log(response.data);
   let cityElement = document.querySelector("#city-name");
   let currentCity = response.data.city;
   let currentCountry = response.data.country;
   let currentTemperatureElement = document.querySelector("#todays-temp");
   let currentTemperature = response.data.temperature.current;
+  let currentFeelsElement = document.querySelector("#today-feels-like");
+  let currentFeelsLike = response.data.temperature.feels_like;
+  let currentdescriptionElement = document.querySelector("#todays-description");
+  let currentDescription = response.data.condition.description;
+  let currentWindSpeedElement = document.querySelector("#wind-speed-today");
+  let currentWindSpeed = response.data.wind.speed;
+  let currentWindDirectionElement =
+    document.querySelector("#wind-degree-today");
+  let currentWindDirection = response.data.wind.degree;
+  let currentHumidityElement = document.querySelector("#humidity-today");
+  let currentHumidity = response.data.temperature.humidity;
+  let currentPressureElement = document.querySelector("#pressure-today");
+  let currentPressure = response.data.temperature.pressure;
 
   cityElement.innerHTML = `${currentCity}, ${currentCountry}`;
   currentTemperatureElement.innerHTML = Math.round(currentTemperature);
+  currentFeelsElement.innerHTML = Math.round(currentFeelsLike);
+  currentdescriptionElement.innerHTML = `${currentDescription}`;
+  currentWindSpeedElement.innerHTML = `${currentWindSpeed}Km/h`;
+  currentWindDirectionElement.innerHTML = currentWindDirection;
+  currentHumidityElement.innerHTML = `${currentHumidity}%`;
+  currentPressureElement.innerHTML = currentPressure;
 }
 
 function searchCity(city) {
